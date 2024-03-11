a9dea74545bfd0d3f36e60dc6aebec3db9b23954
hianshul07 <hianshul07@gmail.com>
2024-03-11 23:34:58 +0530

    UI changes

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	Keyboard,
+	KeyboardAvoidingView,
+	Platform,
+	ScrollView,
+	StyleSheet,
+	Text,
+	// TextInput,
+	Pressable,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import { PaperProvider, TextInput, Appbar, Button, FAB } from 'react-native-paper';
 import Task from './components/Task';
 
 export default function App() {
+	const [task, setTask] = useState('');
+	const [taskItems, setTaskItems] = useState([]);
+
+	const handleAddTask = () => {
+		Keyboard.dismiss();
+		setTaskItems([...taskItems, task]), setTask(null);
+	};
+
+	const completeTask = (index) => {
+		let itemsCopy = [...taskItems];
+		itemsCopy.splice(index, 1);
+		setTaskItems(itemsCopy);
+	};
+
 	return (
-		<View style={styles.container}>
-			<StatusBar style='auto' />
-			<View style={styles.tasksWrapper}>
-				<Text style={styles.sectionTitle}>Tasks</Text>
-				<View style={styles.items}></View>
+		<PaperProvider>
+			<Appbar.Header >
+       <Appbar.Content title="Tasks" style={styles.sectionTitle}/>
+    </Appbar.Header>
+			<View style={styles.container}>
+				<ScrollView
+					contentContainerStyle={{ flexGrow: 1 }}
+					keyboardShouldPersistTaps='handled'
+				>
+					<View style={styles.tasksWrapper}>
+						<View style={styles.items}>
+							{taskItems.map((item, index) => {
+								return (
+									<Pressable key={index} onPress={() => completeTask(index)}>
+										<Task text={item} />;
+									</Pressable>
+								);
+							})}
+						</View>
+					</View>
+				</ScrollView>
+				<KeyboardAvoidingView
+					behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+					style={styles.writeTaskWrapper}
+				>
+					<TextInput
+						style={styles.input}
+						label="Enter task"
+						multiline={true}
+						// placeholder='write a task'
+						mode='outlined'
+						value={task}
+						onChangeText={(text) => setTask(text)}
+					/>
+					<FAB style={styles.addButton} size='medium' icon="plus" mode="flat" onPress={() => handleAddTask()} />
+				</KeyboardAvoidingView>
 			</View>
-			<Task text="car"/>
-			<Task text="cat"/>
-			<Task text="orange"/>
-			<Task text="dog"/>
-			<Task text="vroom"/>
-		</View>
+		</PaperProvider>
 	);
 }
 
@@ -23,17 +76,38 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#E8EAED',
-    // paddingHorizontal: 32
-    paddingHorizontal: 20,
+		// paddingHorizontal: 32
+		paddingHorizontal: 20,
+		alignItems: 'center'
 	},
 	tasksWrapper: {
 		paddingTop: 62,
 	},
-	sectionTitle: {
-		// color: '#fff',
-		fontSize: 32,
-		fontWeight: 'bold',
+	items: {
+		marginTop: 30,
 	},
-	items: {},
+	writeTaskWrapper: {
+		// position: 'absolute',
+		bottom: 48,
+		width: '100%',
+		flexDirection: 'row',
+		
+		justifyContent: 'space-between',
+		// alignItems: 'center',
+	},
+	input: {
+		// paddingVertical: 8,
+		// marginHorizontal: 16,
+		backgroundColor: '#FFF',
+		// borderColor: '#C0C0C0',
+		// borderWidth: 1,
+		borderRadius: 12,
+		width: '80%',
+	},
+	addButton: {
+		alignItems: "center",
+		justifyContent: "center",
+		
+	},
 });
 
--- components/Task.jsx
+++ components/Task.jsx
@@ -1,10 +1,10 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const Task = ({ text }) => {
+const Task = ({text}) => {
 	return (
 		<View style={styles.item}>
 			<View style={styles.itemLeft}>
-				<TouchableOpacity style={styles.square}></TouchableOpacity>
+				<View style={styles.square}></View>
 				<Text style={styles.itemText}>{text}</Text>
 			</View>
 			<View style={styles.circular}></View>
@@ -15,6 +15,7 @@ const Task = ({ text }) => {
 const styles = StyleSheet.create({
 	item: {
     backgroundColor: '#FFF',
+    width: '100%',
     padding: 15,
     borderRadius: 10,
     flexDirection: 'row',
